71ade169c810513bd6f321657a65f6629d37155f
DIPAK Muthe <129575863+dipakmuthe@users.noreply.github.com>
2023-07-02 12:50:12 +0530

    Random Number generating code

---
+++ Random_num.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],i;
+    printf("Enter limit :");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {a[i]=rand();
+    }
+    printf("\n Random Numbers :");
+    for(i=0;i<n;i++)
+    printf("%d\t",a[i]);
+}
